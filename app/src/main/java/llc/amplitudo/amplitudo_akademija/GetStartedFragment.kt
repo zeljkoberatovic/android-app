ac07b9ff4a79ea49685bf5fdafe9fc44cfaa37f5
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2023-01-18 11:16:36 +0100

    creates fragments and adds jetpack navigation component dependency

---
+++ app/src/main/java/llc/amplitudo/amplitudo_akademija/GetStartedFragment.kt
@@ -0,0 +1,26 @@
+package llc.amplitudo.amplitudo_akademija
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class GetStartedFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_get_started, container, false)
+    }
+
+}
